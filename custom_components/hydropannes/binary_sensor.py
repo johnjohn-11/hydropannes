899a521808d243a6e76d6204044d08361c715bed
johnjohn-11 <75040736+johnjohn-11@users.noreply.github.com>
2026-04-20 15:12:08 -0400

    refactor: Update binary_sensor.py

--- custom_components/hydropannes/binary_sensor.py
+++ custom_components/hydropannes/binary_sensor.py
@@ -3,21 +3,23 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
     BinarySensorEntity,
 )
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import EntityCategory
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
-from .coordinator import HydroPannesDataUpdateCoordinator
 from .helpers import HydroPannesHelperMixin
+
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+    from .coordinator import HydroPannesDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
